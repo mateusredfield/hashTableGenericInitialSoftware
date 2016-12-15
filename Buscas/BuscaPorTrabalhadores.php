04b9b4896d8cf2985367cab1bc2d1c15305fa7c3
Mateus <mateuscampari@gmail.com>
2016-12-15 19:48:36 -0200

    Inclusao das buscas e da pagina de buscas - Ainda faltam dois tipos de buscas

---
+++ Buscas/BuscaPorTrabalhadores.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once(__DIR__.'/../resources/support/variables.php');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+//Initiations
+$cpf = $_POST['cpf'];
+$sql = "SELECT * FROM trabalhadores WHERE CPF = '$cpf' ";
+$result = $conn->query($sql);
+
+//Query, fetch, and echo
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $response[] = $row;
+    }
+    echo (json_encode($response));
+} else {
+    echo(json_encode( array('0' => 'No Elements') ));
+}
+
+$conn->close();
+?>
