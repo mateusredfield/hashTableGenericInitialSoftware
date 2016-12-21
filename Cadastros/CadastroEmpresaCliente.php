a8d5c144b4f0860f3d18e4b89f9187634d4bcb02
Mateus <mateuscampari@gmail.com>
2016-12-21 02:52:38 -0200

    Correções de padrões de expressões regulares e correção da formula do brPdh

--- Cadastros/CadastroEmpresaCliente.php
+++ Cadastros/CadastroEmpresaCliente.php
@@ -2,18 +2,6 @@
 
 require_once(__DIR__.'/../resources/support/variables.php');
 
-//Dados do servidor local
-//$servername = "localhost";
-//$username = "root";
-//$password = "";
-//$dbname = "dbTeste";
-
-//Dados servidor em producao
-// $servername = "fdb3.leadhoster.com";
-// $username = "786484_mateus";
-// $password = "MDS_cbattle2015";
-// $dbname = "786484_mateus";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
